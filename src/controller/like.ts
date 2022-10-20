0ccfe948da37f0e559fc9b20bd9f17a3a724459b
NagyeongHa <hnk3176@naver.com>
2022-10-20 23:35:10 +0900

    Add: Like API - 좋아요여부, 좋아요 취소, 좋아요한 게시글 모아보기
    [GET] /like/check/:post_id/:sns_id
    
    [DELETE] /like
    Request - sns_id, post_id
    
    [GET] /like/:sns_id

--- src/controller/like.ts
+++ src/controller/like.ts
@@ -1,8 +1,34 @@
 import { Request, Response } from "express";
 import * as LikeModel from "../models/like";
 
+//좋아요 +1
 export const upLike = async (req: Request, res: Response) => {
-  const like = req.body;
-  const data = LikeModel.upLike(like);
+  const likeParam = req.body; //sns_id, post_Id
+  const data = await LikeModel.upLike(likeParam);
   res.status(201).json(data);
+};
+
+//좋아요 -1
+export const unLike = async (req: Request, res: Response) => {
+  const likeParam = req.body; //sns_id, post_Id
+  await LikeModel.unLike(likeParam);
+  res.status(200).end();
+};
+
+//좋아요 개수 및 좋아요 여부
+export const findCountAndAlreadyLiked = async (req: Request, res: Response) => {
+  const post_id: number = parseInt(req.params.post_id);
+  const { sns_id } = req.params;
+
+  const countLikes = await LikeModel.countLikes(post_id);
+  const alreadyLiked = await LikeModel.alreadyLiked({ post_id, sns_id });
+  const data = { total: countLikes, alreadyLiked };
+  res.status(200).json(data);
+};
+
+//내가 좋아요한 게시글 모아보기
+export const findLikePostBySnsId = async (req: Request, res: Response) => {
+  const { sns_id } = req.params;
+  const data = await LikeModel.findLikePostBySnsId(sns_id);
+  res.status(200).json(data);
 };
--- src/routes/like.ts
+++ src/routes/like.ts
@@ -6,4 +6,13 @@ const router = express.Router();
 //좋아요 +1
 router.post("/", controller.upLike);
 
+//좋아요 -1
+router.delete("/", controller.unLike);
+
+//posts에 보낼 like 데이터
+router.get("/check/:post_id/:sns_id", controller.findCountAndAlreadyLiked);
+
+//내가 좋아요한 게시글 모아보기
+router.get("/:sns_id", controller.findLikePostBySnsId);
+
 export default router;
